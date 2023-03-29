d17e4e8632a307afaa32d2fe9ba3ee8b41062353
wingo-dev <rukshanlancer@gmail.com>
2023-03-29 17:57:39 +0800

    encapsulation with class

--- index.php
+++ index.php
@@ -1,44 +1,13 @@
 <?php
+require_once('s3.php');
 
-require_once('config.php');
-
-// Include the AWS SDK for PHP
-require 'vendor/autoload.php';
-
-use Aws\S3\S3Client;
-use Aws\Exception\AwsException;
-
-// Set your AWS credentials
-$credentials = new Aws\Credentials\Credentials(ACCESS_KEY, SECRET_KEY);
-
-// Set your region and bucket name
-$region = 'eu-central-1'; // Update with your region
-$bucket = 'gp-site'; // Update with your bucket name
-
-// Create an S3Client instance
-$s3 = new S3Client([
-    'version' => 'latest',
-    'region'  => $region,
-    'credentials' => $credentials
-]);
-
-// Set the image file path and name
-$imageFilePath = './img/2.jpg'; // Update with your image file path
-$imageFileName = 'test.jpg'; // Update with your image file name
-
-// Upload the image file to S3
-try {
-    $s3->putObject([
-        'Bucket' => $bucket,
-        'Key'    => $imageFileName,
-        'SourceFile' => $imageFilePath,
-        'ACL'    => 'public-read', // Set the file permission to public-read
-    ]);
+$uploader = new S3Uploader('eu-central-1', 'gp-site');
+$result = $uploader->uploadImage('./img/2.jpg', './layout/test.jpg');
+if ($result) {
     echo "Image uploaded successfully.";
-} catch (AwsException $e) {
-    echo "Error uploading image: " . $e->getMessage();
+} else {
+    echo "Error uploading image.";
 }
+$url = $uploader->getImageUrl('./layout/test.jpg');
 
-/*
-it helped me to configure the bucket permission to upload image
-https://stackoverflow.com/questions/70333681/for-an-amazon-s3-bucket-deployment-from-github-how-do-i-fix-the-error-accesscont*/
+echo "<img src='$url'/>";
--- s3.php
+++ s3.php
@@ -1,67 +1,52 @@
 <?php
+require_once('config.php');
+require 'vendor/autoload.php';
+
+use Aws\S3\S3Client;
+use Aws\Exception\AwsException;
+use Aws\Credentials\Credentials;
+
 class S3Uploader
 {
-    private $awsAccessKey;
-    private $awsSecretKey;
-    private $bucketName;
-    private $region;
-    private $s3Endpoint;
 
-    public function __construct($awsAccessKey, $awsSecretKey, $bucketName, $region, $s3Endpoint)
+    private $s3;
+    private $region;
+    private $bucket;
+
+    public function __construct($region, $bucket)
     {
-        $this->awsAccessKey = $awsAccessKey;
-        $this->awsSecretKey = $awsSecretKey;
-        $this->bucketName = $bucketName;
         $this->region = $region;
-        $this->s3Endpoint = $s3Endpoint;
+        $this->bucket = $bucket;
+        $credentials = new Credentials(ACCESS_KEY, SECRET_KEY);
+        $this->s3 = new S3Client([
+            'version' => 'latest',
+            'region'  => $this->region,
+            'credentials' => $credentials
+        ]);
     }
 
-    public function uploadFileToS3($filePath, $fileKey)
+    public function uploadImage($imageFilePath, $imageFileName)
     {
-        $date = gmdate('Ymd\THis\Z');
-        $shortDate = gmdate('Ymd');
-        $credentialScope = $shortDate . '/' . $this->region . '/s3/aws4_request';
+        try {
+            $this->s3->putObject([
+                'Bucket' => $this->bucket,
+                'Key'    => $imageFileName,
+                'SourceFile' => $imageFilePath,
+                'ACL'    => 'public-read',
+            ]);
+            return true;
+        } catch (AwsException $e) {
+            return false;
+        }
+    }
 
-        $contentType = mime_content_type($filePath);
-        $contentMD5 = base64_encode(md5_file($filePath, true));
-        $contentSha256 = hash_file('sha256', $filePath);
-
-        $canonicalizedAmzHeaders = "x-amz-acl:public-read\n";
-        $canonicalizedResource = "/" . $this->bucketName . "/" . $fileKey;
-
-        $stringToSign = "PUT\n$canonicalizedResource\n$date\n$contentType\n$canonicalizedAmzHeaders x-amz-content-sha256:$contentSha256\nx-amz-acl:public-read\nhost:" . $this->bucketName . '.' . $this->s3Endpoint . "\n\nx-amz-acl;host;x-amz-content-sha256;x-amz-date\n$contentSha256";
-
-        $kDate = hash_hmac('sha256', $shortDate, 'AWS4' . $this->awsSecretKey, true);
-        $kRegion = hash_hmac('sha256', $this->region, $kDate, true);
-        $kService = hash_hmac('sha256', 's3', $kRegion, true);
-        $kSigning = hash_hmac('sha256', 'aws4_request', $kService, true);
-
-        $signature = hash_hmac('sha256', $stringToSign, $kSigning);
-
-        $headers = array(
-            'Host: ' . $this->bucketName . '.' . $this->s3Endpoint,
-            'Content-Type: ' . $contentType,
-            'Content-MD5: ' . $contentMD5,
-            'x-amz-acl: public-read',
-            'x-amz-date: ' . $date,
-            'x-amz-content-sha256: ' . $contentSha256,
-            'Authorization: AWS4-HMAC-SHA256 Credential=' . $this->awsAccessKey . '/' . $credentialScope . ', SignedHeaders=host;content-type;content-md5;x-amz-acl;x-amz-content-sha256;x-amz-date, Signature=' . $signature,
-        );
-
-        $url = 'https://' . $this->bucketName . '.' . $this->s3Endpoint . '/' . $fileKey;
-
-        $curl = curl_init();
-
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($curl, CURLOPT_PUT, true);
-        curl_setopt($curl, CURLOPT_INFILE, fopen($filePath, 'r'));
-
-        $response = curl_exec($curl);
-
-        curl_close($curl);
-
-        return $response;
+    public function getImageUrl($imageFileName)
+    {
+        try {
+            $url = $this->s3->getObjectUrl($this->bucket, $imageFileName);
+            return $url;
+        } catch (AwsException $e) {
+            return false;
+        }
     }
 }
